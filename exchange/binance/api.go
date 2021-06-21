9bcf3cd95b86bd915a055db5bda9c80a1205ab1c
kasaderos <kberda99@gmail.com>
2021-06-21 17:07:39 +0000

    binance: fix create order

--- exchange/binance/api.go
+++ exchange/binance/api.go
@@ -174,10 +174,6 @@ func (ex *binance) CreateOrder(ctx context.Context, order *exchange.Order) (id s
 	query.Add("symbol", pair)
 	query.Add("side", order.Side)
 	query.Add("type", order.Type)
-	query.Add("timeInForce", "GTC")
-	// if stopLoss > 0 {
-	// 	query.Add("stopPrice", strconv.FormatFloat(stopLoss, 'f', -1, 64))
-	// }
 	query.Add("timestamp", timestamp)
 	query.Add("price", strconv.FormatFloat(order.Price, 'f', -1, 64))
 	if order.Side == "SELL" {
